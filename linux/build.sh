39db0ccd6dcb27d9b4a321ae9bafefc8d10080e5
apoduval <apoduval@node0.memtis1.scailmemorywall-pg0.wisc.cloudlab.us>
2025-10-05 19:57:15 -0500

    Update build script

--- linux/build.sh
+++ linux/build.sh
@@ -1,3 +1,4 @@
+sudo apt install -y msr-tools sudo build-essential libncurses-dev bison flex libssl-dev libelf-dev fakeroot dwarves
 cp /boot/config-$(uname -r) .config
 scripts/config --disable SYSTEM_TRUSTED_KEYS
 scripts/config --disable SYSTEM_REVOCATION_KEYS
@@ -6,5 +7,6 @@ echo "CONFIG_HTMM=y" >> .config
 scripts/config --disable CONFIG_DEBUG_INFO_BTF
 yes "" | make oldconfig
 scripts/config --disable CONFIG_DEBUG_INFO_BTF
+scripts/config -m CONFIG_X86_MSR
 make -j40;  sudo make modules_install -j$(nproc); sudo make install
-
+sudo reboot
