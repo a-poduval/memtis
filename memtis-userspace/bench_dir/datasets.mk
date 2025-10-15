c9cc77b49863aa3eddfd14e1c406b0c5cd4e2bc3
apoduval <apoduval@node0.memtis.scailmemorywall-pg0.wisc.cloudlab.us>
2025-10-15 15:06:03 -0500

    Update datasets.mk to use kddb instead of kdd12.

--- memtis-userspace/bench_dir/datasets.mk
+++ memtis-userspace/bench_dir/datasets.mk
@@ -4,13 +4,13 @@
 DATASET_DIR = datasets/
 
 .PHONY: dataset
-dataset: ${DATASET_DIR} ${DATASET_DIR}/kdd12
+dataset: ${DATASET_DIR} ${DATASET_DIR}/kddb
 
 ${DATASET_DIR}:
 	mkdir -p $@
 
-${DATASET_DIR}/kdd12: ${DATASET_DIR}/kdd12.xz
-	xz -d $^
+${DATASET_DIR}/kddb: ${DATASET_DIR}/kddb.bz2
+	bunzip2 $^
 
-${DATASET_DIR}/kdd12.xz:
-	wget -P ${DATASET_DIR} https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kdd12.xz
+${DATASET_DIR}/kddb.xz:
+	wget -P ${DATASET_DIR} https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kddb.bz2
